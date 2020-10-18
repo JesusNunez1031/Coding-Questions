c008987d6ed493f176da50cda41951cb04930e7a
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2020-10-17 23:12:15 -0700

    updates

---
+++ LeetCode/LinkedList/oddEven.java
@@ -0,0 +1,44 @@
+public class oddEven {
+    /*
+        Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+        You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+        Example 1:
+        Input: 1->2->3->4->5->NULL
+        Output: 1->3->5->2->4->NULL
+
+        Example 2:
+        Input: 2->1->3->5->6->4->7->NULL
+        Output: 2->3->6->7->1->5->4->NULL
+     */
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        //List of odd nodes
+        ListNode odd = head;
+        ListNode ans = odd;
+
+        //List of even Nodes
+        ListNode even = odd.next;
+
+        //head of the evens used for reconnection to odds
+        ListNode evenHead = even;
+
+        while (odd.next != null && even.next != null) {
+            //Point the odd node to the node after the even node and move its pointer forward
+            odd.next = even.next;
+            odd = odd.next;
+
+            //The even nodes next node will be the node next to the odd so we point it there
+            even.next = odd.next;
+            even = even.next;
+        }
+        //connect the end of the odds to the start of the even list
+        odd.next = evenHead;
+
+        return ans;
+    }
+}
