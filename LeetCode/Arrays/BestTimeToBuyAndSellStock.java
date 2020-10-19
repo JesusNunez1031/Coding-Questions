bb6af71f742cac7280c980ea4475ca4ddcf91a78
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2020-10-19 00:15:48 -0700

    New LeetCode HM

--- LeetCode/Arrays/BestTimeToBuyAndSellStock.java
+++ LeetCode/Arrays/BestTimeToBuyAndSellStock.java
@@ -2,9 +2,7 @@ public class BestTimeToBuyAndSellStock {
 
     /*
     Say you have an array for which the ith element is the price of a given stock on day i.
-
     If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
-
     Note that you cannot sell a stock before you buy one.
 
     Example 1:
@@ -19,15 +17,15 @@ public class BestTimeToBuyAndSellStock {
         int min_val = Integer.MAX_VALUE;
         int max_profit = 0;
 
-        for(int i = 0;i < prices.length;i++) {
+        for (int price : prices) {
             /*Get the smallest value possible as we iterate through array, if the array happens to be in descending order, there is no way to get a max_profit,
               since we would always sell for a loss
              */
-            if(prices[i] < min_val) {
-                min_val = prices[i];
+            if (price < min_val) {
+                min_val = price;
                 //If the current value is - our current min_val is greater than the current max profit, make that subtraction our new max
-            } else if(prices[i] - min_val > max_profit) {
-                max_profit = prices[i] - min_val;
+            } else if (price - min_val > max_profit) {
+                max_profit = price - min_val;
             }
         }
         return max_profit;
---
+++ LeetCode/Hash Maps/ContainsDuplicatesII.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicatesII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums.length < 2 || k <= 0) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!(map.containsKey(nums[i]))) {
+                map.put(nums[i], i);
+            } else {
+                int j = map.get(nums[i]);
+                if (i != j && Math.abs(i - j) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+        }
+        return false;
+    }
+}
