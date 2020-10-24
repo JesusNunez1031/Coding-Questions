10beb8a1bd61584efa77730aacf6c471c7792d26
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2020-10-24 01:40:04 -0700

    New CodingBat

---
+++ CodingBat/Functional-1/noX.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class noX {
+    public List<String> noX(List<String> strings) {
+        return strings.stream()
+                .map(s -> s.replaceAll("x", ""))
+                .collect(Collectors.toList());
+    }
+
+}
