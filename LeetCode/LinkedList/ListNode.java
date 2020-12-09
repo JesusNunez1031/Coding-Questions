39af7c3333d2526f0f405b24c1b98abb070ba1ce
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2020-12-08 23:12:37 -0800

    LL

--- LeetCode/LinkedList/ListNode.java
+++ LeetCode/LinkedList/ListNode.java
@@ -27,4 +27,17 @@ public class ListNode {
             iter = iter.next;
         }
     }
+
+    //method to return a generic list given a size
+    public ListNode makeList(int size){
+        ListNode head = new ListNode(1);
+        ListNode iter = head;
+        int i = 2;
+        while (size > 1) {
+            iter.next = new ListNode(i++);
+            iter = iter.next;
+            size--;
+        }
+        return head;
+    }
 }
--- LeetCode/LinkedList/swapNodesInPairs.java
+++ LeetCode/LinkedList/swapNodesInPairs.java
@@ -4,36 +4,81 @@ public class swapNodesInPairs {
     Given a linked list, swap every two adjacent nodes and return its head.
     You may not modify the values in the list's nodes. Only nodes itself may be changed.
 
-     */
-    public ListNode swapPairs(ListNode head) {
-        ListNode temp = new ListNode(0);
-        temp.next = head;
+    Example 1:
+    Original: 1 -> 2 -> 3 -> 4
+    Swapped: 2 -> 1 -> 4 -> 3
+    Input: head = [1,2,3,4]
+    Output: [2,1,4,3]
 
-        //pointer used to iterate through the list, we do .next since the first node of temp is 0
-        ListNode current = temp.next;
+    Example 2:
+    Input: head = []
+    Output: []
+
+    Example 3:
+    Input: head = [1]
+    Output: [1]
+
+    Constraints:
+        The number of nodes in the list is in the range [0, 100].
+        0 <= Node.val <= 100
+     */
+    /*                                                   Pseudocode
+       1. we want to swap "first" with "second", so we take the nodes .next from "second" and append them to "first" so
+          that "first" is now in the place where "second" used to be
+            Ex: 1 -> 2 -> 3 -> 4
+                first_node = 1
+                second_node = 2
+          we want to swap 1 and 2 so by making first_node.next = second.next, we get 1 -> 3 -> 4, the 1 is now in the right
+          place, now we need to place the 2 before the 1
+
+       2. "current" is our iterator, so its next will be the node that was just swapped so we make it point the the "second_node"
+            Ex:
+                    -1 -> 2 -> 3 -> 4
+                     ↑    ↑
+           current(dummy) second
+
+       3. both nodes have been swapped so all that is left is to combine the lists so we set the next of "second" to "first"
+             Ex: list is now -1 -> 2 -> 1 -> 3 -> 4
+                              ↑    ↑
+                   current(dummy) second
+       4. move the iterator by 2, -1 -> 2 -> 1 -> 3 -> 4
+                                             ↑
+                                       current(dummy)
+    */
+    //TC: O(n) time and constant space
+    private static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode swapped = new ListNode(-1);
+        swapped.next = head;    //append the entire list to the next of swapped
+
+        ListNode current = swapped;
 
         //Move through list two nodes at a time
         while (current.next != null && current.next.next != null) {
-            //Get the two first nodes in list
+
+            //Get the two nodes to be swapped
             ListNode first_node = current.next;
             ListNode second_node = current.next.next;
 
-            //make the first nodes next the second nodes next
+            //place the first_node in the place of the second_node
             first_node.next = second_node.next;
-            //Make the first node the second node
+
+            //place the second_node in the place of the first_node
             current.next = second_node;
 
-            //point the first node, previously the second node, to the rest of the list where its head is the old first node
+            //now combine the two nodes back into the main list
             current.next.next = first_node;
 
-            //move current to the third node
+            //move the iter to the next two nodes to be swapped
             current = current.next.next;
         }
-        return temp.next;
+        return swapped.next;
     }
 
-    //Recursive solution
-    public ListNode swapPairsRec(ListNode head) {
+    //Recursive solution, O(n) space is used due to recursive stack
+    private static ListNode swapPairsRec(ListNode head) {
         if ((head == null) || (head.next == null))
             return head;
         //Set n to the node after the initial head
@@ -43,5 +88,11 @@ public class swapNodesInPairs {
         //point n back to the rest of the list
         n.next = head;
         return n;
+    }
+
+    public static void main(String[] args) {
+        ListNode list = new ListNode().makeList(4);
+        ListNode head = swapPairs(list);
+        head.print(head);
     }
 }
