c2bcb5053efcbcb7c9ecb8cc4a2ff487d4d6306b
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-01-22 17:43:05 -0800

    Greedy

--- LeetCode/Greedy Algorithms/jumpGame.java
+++ LeetCode/Greedy Algorithms/jumpGame.java
@@ -41,7 +41,7 @@ public class jumpGame {
     }
 
     public static void main(String[] args) {
-        int[] arr = {9, 4, 2, 1, 0, 2, 0};
+        int[] arr = {2,3,1,1,4};
         System.out.println(canJump(arr));
     }
 }
