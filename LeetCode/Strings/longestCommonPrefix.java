e01d7863aaa6e16c43942fb197ac0b8e934c2ef5
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-02-02 20:39:45 -0800

    Math & Strings

--- LeetCode/Strings/longestCommonPrefix.java
+++ LeetCode/Strings/longestCommonPrefix.java
@@ -1,17 +1,33 @@
 public class longestCommonPrefix {
+    /*
+    Write a function to find the longest common prefix string amongst an array of strings.
+    If there is no common prefix, return an empty string "".
 
+    Example 1:
+    Input: strs = ["flower","flow","flight"]
+    Output: "fl"
+
+    Example 2:
+    Input: strs = ["dog","racecar","car"]
+    Output: ""
+    Explanation: There is no common prefix among the input strings.
+
+    Constraints:
+        0 <= strs.length <= 200
+        0 <= strs[i].length <= 200
+        strs[i] consists of only lower-case English letters.
+     */
+
+    //TC: O(n)
     public static String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) {
             return "";
         }
-
-        //Set prefix to the first value in array
-        //We make the first value the prefix since its letters will be in the longest prefix
+        //The first word in strs must contain the prefix so we start with a prefix of strs[0]
         String prefix = strs[0];
 
+        //for every word in the array, we reduce the prefix until "prefix" is found in strs[i]
         for (int i = 1; i < strs.length; i++) {
-            //if the prefix is not at the beginning of the next word, reduce prefix by one
-            //We do this until we have a match, otherwise
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
             }
@@ -21,6 +37,23 @@ public class longestCommonPrefix {
             }
         }
         return prefix;
+    }
+
+    //Method using a string builder object, O(n) time and space
+    public String longestCommonPrefixEz(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+
+        //the first string must include the prefix so we set it as the prefix
+        StringBuilder prefix = new StringBuilder(strs[0]);
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix.toString()) != 0) {
+                prefix.setLength(prefix.length() - 1);
+            }
+        }
+        return prefix.toString();
     }
 
     public static void main(String[] args) {
