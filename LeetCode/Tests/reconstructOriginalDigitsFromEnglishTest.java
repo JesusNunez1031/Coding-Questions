50dc2dc8c465d704cc61c238535f929090a23e24
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-03-28 18:24:43 -0700

    Strings

---
+++ LeetCode/Tests/reconstructOriginalDigitsFromEnglishTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class reconstructOriginalDigitsFromEnglishTest {
+    reconstructOriginalDigitsFromEnglish driver = new reconstructOriginalDigitsFromEnglish();
+
+    @Test
+    @DisplayName("String has one digit")
+    void OneDigit() {
+        String s = "ozre";
+        String digits = driver.originalDigits(s);
+        assertEquals(digits, "0");
+        System.out.println(s + ": " + digits);
+    }
+
+    @Test
+    @DisplayName("String has two digits")
+    void TwoDigits() {
+        String s = "ozrenoe";
+        String digits = driver.originalDigits(s);
+        assertEquals(digits, "01");
+        System.out.println(s + ": " + digits);
+    }
+
+    @Test
+    @DisplayName("String has three digits")
+    void ThreeDigits() {
+        String s = "otzrewnooe";
+        String digits = driver.originalDigits(s);
+        assertEquals(digits, "012");
+        System.out.println(s + ": " + digits);
+    }
+
+    @Test
+    @DisplayName("String has four digits")
+    void FourDigits() {
+        String s = "eeotzrterwnohoe";
+        String digits = driver.originalDigits(s);
+        assertEquals(digits, "0123");
+        System.out.println(s + ": " + digits);
+    }
+
+    @Test
+    @DisplayName("String has five digits")
+    void FiveDigits() {
+        String s = "eueortzfrtoerwnohoe";
+        String digits = driver.originalDigits(s);
+        assertEquals(digits, "01234");
+        System.out.println(s + ": " + digits);
+    }
+}
