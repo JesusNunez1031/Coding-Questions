b1da3a861c2b4f7235123dc7aa8f183cfa11a84d
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-04-27 15:14:51 -0700

    Math

--- LeetCode/Math/powersOfThree.java
+++ LeetCode/Math/powersOfThree.java
@@ -1,7 +1,8 @@
 public class powersOfThree {
     /*
     Given an integer n, return true if it is a power of three. Otherwise, return false.
-    An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+    An integer n is a power of three, if there exists an integer x such that n == 3^x.
 
     Example 1:
     Input: n = 27
@@ -20,12 +21,12 @@ public class powersOfThree {
     Output: false
 
     Constraints:
-         -231 <= n <= 231 - 1
+        -2^31 <= n <= 2^31 - 1
+
+
+    Follow up: Could you solve it without loops/recursion?
      */
-    // public boolean isPowerOfThree(int n) {
-    //     //Knowing the limitation of n, we can now deduce that the maximum value of n that is also a power of three is 1162261467
-    //     return n > 0 && 1162261467 % n == 0;
-    // }
+    //TC: O(log3(n)) base 3 since we are looking for powers of 3 so we divide n by 3 at every step
     public boolean isPowerOfThree(int n) {
         if (n < 1) {
             return false;
@@ -36,4 +37,13 @@ public class powersOfThree {
         }
         return n == 1;
     }
+
+    //Follow up:
+     public boolean isPowerOfThreeEz(int n) {
+         /*
+            Knowing the limitation of n, we can now deduce that the maximum value of n that is also a power of three is
+            1162261467, 3 ^ ⌊ log3 (INTEGER.MAX_VALUE) ⌋ = 3 ⌊19.56⌋ = 3 ^ 19 = 1162261467
+          */
+         return n > 0 && 1162261467 % n == 0;
+     }
 }
