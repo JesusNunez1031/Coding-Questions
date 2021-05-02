660c7d56543d00a93c99d7f8ab0e97f6e1d9a381
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-05-02 16:31:54 -0700

    Greedy

---
+++ LeetCode/Tests/courseScheduleIIITest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class courseScheduleIIITest {
+    courseScheduleIII driver = new courseScheduleIII();
+
+    @Test
+    @DisplayName("0 courses")
+    void scheduleCourse0() {
+        int[][] courses = {};
+        assertEquals(0, driver.scheduleCourse(courses));
+    }
+
+    @Test
+    @DisplayName("1 course")
+    void scheduleCourse1() {
+        int[][] courses = {{3, 6}};
+        assertEquals(1, driver.scheduleCourse(courses));
+    }
+
+    @Test
+    @DisplayName("2 courses")
+    void scheduleCourse2() {
+        int[][] courses = {{3, 2}, {4, 3}};
+        assertEquals(0, driver.scheduleCourse(courses));
+    }
+
+    @Test
+    @DisplayName("4 courses")
+    void scheduleCourse4() {
+        int[][] courses = {{3, 5}, {2, 6}, {10, 20}, {20, 35}};
+        assertEquals(4, driver.scheduleCourse(courses));
+    }
+
+    @Test
+    @DisplayName("8 courses")
+    void scheduleCourse8() {
+        int[][] courses = {{3, 5}, {2, 6}, {10, 20}, {20, 35}, {30, 50}, {14, 55}, {20, 80}, {10, 100}};
+        assertEquals(7, driver.scheduleCourse(courses));
+    }
+}
