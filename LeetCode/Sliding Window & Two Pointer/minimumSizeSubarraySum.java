a795a7f8f50bf9e5e42578733c75bafd03e9e338
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-05-15 17:44:45 -0700

    Strings & 2ptr

--- LeetCode/Sliding Window & Two Pointer/minimumSizeSubarraySum.java
+++ LeetCode/Sliding Window & Two Pointer/minimumSizeSubarraySum.java
@@ -1,13 +1,30 @@
 public class minimumSizeSubarraySum {
     /*
-    Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of
-    which the sum ≥ s. If there isn't one, return 0 instead.
+    Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous
+    subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no
+    such subarray, return 0 instead.
 
-    Example:
-    Input: s = 7, nums = [2,3,1,2,4,3]
+    Example 1:
+    Input: target = 7, nums = [2,3,1,2,4,3]
     Output: 2
-    Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+    Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+    Example 2:
+    Input: target = 4, nums = [1,4,4]
+    Output: 1
+
+    Example 3:
+    Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+    Output: 0
+
+    Constraints:
+        1 <= target <= 10^9
+        1 <= nums.length <= 10^5
+        1 <= nums[i] <= 10^5
+
+    Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
      */
+    //TC: O(n)
     private int minSubArrayLen(int s, int[] nums) {
         int sum = 0;
         int min_length = Integer.MAX_VALUE;
@@ -16,7 +33,7 @@ public class minimumSizeSubarraySum {
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
 
-            //reduce the sum as long as its >= s
+            //reduce the sum as long as its >= s while also reducing the current window from j to i
             while (sum >= s) {
                 min_length = Math.min(min_length, i - j + 1);
                 sum -= nums[j];
