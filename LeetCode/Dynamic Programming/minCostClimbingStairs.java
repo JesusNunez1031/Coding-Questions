29bafa7234cda19ad5d4f0d452f299bf30aa85a1
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-06-07 21:11:22 -0700

    DP

--- LeetCode/Dynamic Programming/minCostClimbingStairs.java
+++ LeetCode/Dynamic Programming/minCostClimbingStairs.java
@@ -1,7 +1,8 @@
 public class minCostClimbingStairs {
     /*
     On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
-    Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+    Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the
+    floor, and you can either start from the step with index 0, or the step with index 1.
 
     Example 1:
     Input: cost = [10, 15, 20]
@@ -14,13 +15,21 @@ public class minCostClimbingStairs {
     Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
 
     Note:
-    cost will have a length in the range [2, 1000].
-    Every cost[i] will be an integer in the range [0, 999].
+        cost will have a length in the range [2, 1000].
+        Every cost[i] will be an integer in the range [0, 999].
      */
     //TC: O(n) and space
     private static int minCostClimbingStairs(int[] cost) {
+        //2 items are guaranteed in the array hence if the current cost array only has 2 items, return the smallest
+        if(cost.length < 3) {
+            return Math.min(cost[0], cost[1]);
+        }
         int[] dp = new int[cost.length];
-        //place the cost of the each start in the dp array
+
+        /*
+            we can start from either the first value or the second, we will calculate the min cost by using both values,
+            in the end, either dp[n - 1], starting from cost[0], or dp[n - 2], starting from cost[1], will have the smallest cost
+         */
         dp[0] = cost[0];
         dp[1] = cost[1];
 
@@ -28,7 +37,7 @@ public class minCostClimbingStairs {
         while (i < cost.length) {
             /*
                 At every iteration, the new cost up to the current value is found by adding the current cost to
-                the smallest value from before, either from the starting from index 0 or 1
+                the smallest value from before, either from the starting of index 0 or 1
             */
             dp[i] = cost[i] + Math.min(dp[i - 2], dp[i - 1]);
             i++;
