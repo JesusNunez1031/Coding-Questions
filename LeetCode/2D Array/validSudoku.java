684e4e9c4898ad926559e1f6927c8dab9e4c7480
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-08-20 19:00:48 -0700

    2D Array

--- LeetCode/2D Array/validSudoku.java
+++ LeetCode/2D Array/validSudoku.java
@@ -2,6 +2,52 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class validSudoku {
+    /*
+    Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+        Each row must contain the digits 1-9 without repetition.
+        Each column must contain the digits 1-9 without repetition.
+        Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+    Note:
+    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+    Only the filled cells need to be validated according to the mentioned rules.
+
+
+    Example 1:
+    Input: board =
+    [["5","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]
+    Output: true
+
+    Example 2:
+    Input: board =
+    [["8","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]
+    Output: false
+    Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two
+    8's in the top left 3x3 sub-box, it is invalid.
+
+
+    Constraints:
+        board.length == 9
+        board[i].length == 9
+        board[i][j] is a digit or '.'.
+     */
+    //TC: O(n^2)
     public static boolean isValidSudoku(char[][] board) {
 
         Set<String> set = new HashSet<>();
@@ -22,12 +68,20 @@ public class validSudoku {
         return true;
     }
 
-    //More Efficient method, however more space used
+    //More Efficient method, however more space used | TC: O(n^2) SC: O(n^2)
     public boolean isValidSudokuMap(char[][] board) {
         int n = 9;
-        HashSet<Character>[] row = new HashSet[9];
-        HashSet<Character>[] col = new HashSet[9];
+        // A hashset for each row and column in the board, keep track of seen values
+        HashSet<Character>[] row = new HashSet[n];
+        HashSet<Character>[] col = new HashSet[n];
+
+        /*
+            A hashset for each row and column in a sub-box of a board, also used to keep track of seen values. Each cell
+            represents a sub-box
+         */
         HashSet<Character>[][] sub = new HashSet[3][3];
+
+        // initialize hashsets
         for (int i = 0; i < n; i++) {
             row[i] = new HashSet<>();
             col[i] = new HashSet<>();
@@ -37,17 +91,21 @@ public class validSudoku {
                 sub[i][j] = new HashSet<>();
             }
         }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 char c = board[i][j];
                 if (c != '.') {
+                    // calculate the location of the current sub-box
                     int x = (i / 3), y = (j / 3);
+                    //not being able to add to a row, column, or sub-box means we're at a duplicate value hence board is invalid
                     if (!row[i].add(c) || !col[j].add(c) || !sub[x][y].add(c)) {
                         return false;
                     }
                 }
             }
         }
+        // board was valid
         return true;
     }
 
