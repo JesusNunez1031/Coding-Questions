e3790d423c7bcafc945909836f9ed6449536a469
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-10-03 17:53:03 -0700

    2D Array

--- LeetCode/2D Array/islandPerimeter.java
+++ LeetCode/2D Array/islandPerimeter.java
@@ -1,8 +1,14 @@
 public class islandPerimeter {
     /*
-    You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
-    Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
-    The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+    You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents
+    water.
+
+    Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and
+    there is exactly one island (i.e., one or more connected land cells).
+
+    The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell
+    is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter
+    of the island.
 
     Example 1:
     Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
@@ -23,6 +29,7 @@ public class islandPerimeter {
         1 <= row, col <= 100
         grid[i][j] is 0 or 1.
      */
+    //TC: O(m * n)
     public int islandPerimeter(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
@@ -31,22 +38,24 @@ public class islandPerimeter {
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                //if the current value is 1, we assume its a lone square so add 4
+                //if the current value is 1, we assume it's a lone square so add 4
                 if (grid[i][j] == 1) {
                     perimeter += 4;
-                    //check if there is a 1 left to it and if there is, subtract 2 from perimeter
+
+                    // check all sides of the current land mass for connections to other land masses
+                    //left
                     if (j > 0 && grid[i][j - 1] == 1) {
                         perimeter--;
                     }
-                    //check right for a 1, if there is we have one less connection
+                    //right
                     if (j < grid[i].length - 1 && grid[i][j + 1] == 1) {
                         perimeter--;
                     }
-                    //check down for a 1
+                    //down
                     if (i < grid.length - 1 && grid[i + 1][j] == 1) {
                         perimeter--;
                     }
-                    //check up for a 1
+                    //up
                     if (i > 0 && grid[i - 1][j] == 1) {
                         perimeter--;
                     }
@@ -54,14 +63,14 @@ public class islandPerimeter {
                     //--------------------------------------------//
                                 simplified
                       since we know that grid cells are connected horizontally/vertically
-                      (not diagonally) we can just check once side and above for each 1 encountered
+                      (not diagonally) we can just check one side and above for each 1 encountered
                       if (j > 0 && grid[i][j - 1] == 1) {
                         perimeter -= 2;
                       }
                       if (i > 0 && grid[i - 1][j] == 1) {
                          perimeter -= 2;
                       }
-                      we don't have to check all the dimensions of a found 1
+                      we don't have to check all the dimensions when a land mass is found
                      */
                 }
             }
