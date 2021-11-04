4ad7785ad4c12388db268f38439aa539ef37d4ab
JesusNunez1031 <jnunez105@toromail.csudh.edu>
2021-11-03 17:10:57 -0700

    Trees

--- LeetCode/Trees/SumOfLeftLeaves.java
+++ LeetCode/Trees/SumOfLeftLeaves.java
@@ -1,7 +1,7 @@
-public class sumOfLeftLeaves {
+public class SumOfLeftLeaves {
     /*
         Find the sum of all left leaves in a given binary tree.
-        Example:
+        Example 1:
 
             3
            / \
@@ -10,6 +10,14 @@ public class sumOfLeftLeaves {
            15   7
 
         There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+        Example 2:
+        Input: root = [1]
+        Output: 0
+
+        Constraints:
+            The number of nodes in the tree is in the range [1, 1000].
+            -1000 <= Node.val <= 1000
      */
 
     private int sumOfLeaves = 0;
@@ -38,5 +46,18 @@ public class sumOfLeftLeaves {
         }
         //if there are no values to the left and right of the current node it is a leaf
         return root.left == null && root.right == null;
+    }
+
+    // optimized helper method to not use global variables
+    private int findSum(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        if(isLeaf(root.left)) {
+            // if the left node is a leaf return its value + the sum of the right subtrees left nodes
+            return root.left.val + findSum(root.right);
+        }
+        return findSum(root.left) + findSum(root.right);
     }
 }
